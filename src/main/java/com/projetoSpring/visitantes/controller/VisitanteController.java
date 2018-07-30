42e17de23a2da44830fa0ca08e0b46d1a0b74b6f
Leonardo Gomes <leonardogomes2704@gmail.com>
2018-07-30 00:37:03 -0300

    initial commit

--- src/main/java/com/projetoSpring/visitantes/controller/VisitanteController.java
+++ src/main/java/com/projetoSpring/visitantes/controller/VisitanteController.java
@@ -1,8 +1,15 @@
 package com.projetoSpring.visitantes.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class VisitanteController {
+
+	@GetMapping("/visitantes")
+	public String listar() {
+		return "ListaVisitantes";
+	}
+	
 
 }
